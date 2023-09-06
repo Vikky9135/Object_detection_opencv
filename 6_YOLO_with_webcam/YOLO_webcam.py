fb3c2793510ace3153fef6f5b047cdfd6c804255
Vikram <vikramvikky2003@gmail.com>
2023-09-06 18:43:24 +0530

    Detect and draw contours for object using cvzone.

--- 6_YOLO_with_webcam/YOLO_webcam.py
+++ 6_YOLO_with_webcam/YOLO_webcam.py
@@ -1,6 +1,9 @@
 from ultralytics import YOLO
 import cv2
 import cvzone
+import math
+
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
@@ -10,5 +13,20 @@ model = YOLO("../YOLO_weights/yolov8m.pt")
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+            # print(x1, y1, x2, y2)
+            # cv2.rectangle(img, (x1,y1),(x2,y2),(255,0,255),3)
+
+            w,h = x2-x1, y2-y1
+            cvzone.cornerRect(img, (x1,y1,w,h))
+
+
+
     cv2.imshow("Image",img)
     cv2.waitKey(1)
