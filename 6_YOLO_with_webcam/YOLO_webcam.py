dfd36679c496cb685555041f0ea7c014c5ce4f49
Vikram <vikramvikky2003@gmail.com>
2023-09-06 18:54:31 +0530

    Detect object with their names and confidence level on webcam.

--- 6_YOLO_with_webcam/YOLO_webcam.py
+++ 6_YOLO_with_webcam/YOLO_webcam.py
@@ -10,6 +10,18 @@ cap.set(4, 720)
 
 model = YOLO("../YOLO_weights/yolov8m.pt")
 
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
@@ -26,7 +38,13 @@ while True:
             w,h = x2-x1, y2-y1
             cvzone.cornerRect(img, (x1,y1,w,h))
 
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
 
+            # Class Name
+            cls = int(box.cls[0])
+
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
 
     cv2.imshow("Image",img)
     cv2.waitKey(1)
